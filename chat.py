de5484a05c562b29287d4fe41a9fe30e02406740
artem <artem@deffun.io>
2025-01-09 17:44:04 +0700

    extend with fastapi

---
+++ chat.py
@@ -0,0 +1,107 @@
+from typing import Dict, Optional
+
+import chainlit as cl
+from chainlit.input_widget import Select, Switch, Slider, Tags
+
+
+@cl.on_message
+async def on_message(message: cl.Message):
+    user: cl.User = cl.user_session.get("user")
+    token = user.metadata.get("token")
+    print(token)
+
+    # Get all the messages in the conversation in the OpenAI format
+    print(cl.chat_context.to_openai())
+
+    # Send the response
+    response = f"Hello, you just sent: {message.content}!"
+    await cl.Message(response).send()
+
+
+# TODO: add scopes and set up to use cookie or use header when accessing an API
+# noinspection PyUnusedLocal
+@cl.oauth_callback
+def oauth_callback(
+  provider_id: str,
+  token: str,
+  raw_user_data: Dict[str, str],
+  default_user: cl.User,
+) -> Optional[cl.User]:
+    print('raw_user_data')
+    print(raw_user_data)
+    print('default user')
+    print(default_user)
+    default_user.metadata['token'] = token
+    return default_user
+
+
+@cl.set_starters
+async def set_starters():
+    return [
+        cl.Starter(
+            label="Example",
+            message="Find the receipt of my favourite apple pie.",
+            icon="/public/example.png"
+        )
+    ]
+
+
+@cl.on_chat_start
+async def start():
+    settings = cl.ChatSettings(
+        [
+            Select(
+                id="Model",
+                label="OpenAI - Model",
+                values=["gpt-3.5-turbo", "gpt-3.5-turbo-16k", "gpt-4", "gpt-4-32k"],
+                initial_index=1,
+            ),
+            Tags(id="StopSequence", label="OpenAI - StopSequence", initial=["uno", "dos"], values=["uno", "dos", "tres"]),
+            Switch(id="Streaming7", label="OpenAI - Stream Tokens", initial=True),
+            Slider(
+                id="Temperature",
+                label="OpenAI - Temperature",
+                initial=0,
+                min=0,
+                max=2,
+                step=0.1,
+            ),
+            Slider(
+                id="SAI_Steps",
+                label="Stability AI - Steps",
+                initial=30,
+                min=10,
+                max=150,
+                step=1,
+                description="Amount of inference steps performed on image generation.",
+            ),
+            Slider(
+                id="SAI_Cfg_Scale",
+                label="Stability AI - Cfg_Scale",
+                initial=7,
+                min=1,
+                max=35,
+                step=0.1,
+                description="Influences how strongly your generation is guided to match your prompt.",
+            ),
+            Slider(
+                id="SAI_Width",
+                label="Stability AI - Image Width",
+                initial=512,
+                min=256,
+                max=2048,
+                step=64,
+                tooltip="Measured in pixels",
+            ),
+            Slider(
+                id="SAI_Height",
+                label="Stability AI - Image Height",
+                initial=512,
+                min=256,
+                max=2048,
+                step=64,
+                tooltip="Measured in pixels",
+            ),
+        ]
+    )
+    await settings.send()
--- main.py
+++ main.py
@@ -1,100 +1,98 @@
-from typing import Dict, Optional
+from typing import Annotated, Union
+
+from fastapi import FastAPI, Depends, Request
+from fastapi.responses import (
+    HTMLResponse,
+)
+from fastapi.security import APIKeyCookie
+from google.oauth2.credentials import Credentials
+from googleapiclient.discovery import build
+from googleapiclient.errors import HttpError
 
 import chainlit as cl
-from chainlit.input_widget import Select, Switch, Slider, Tags
+from chainlit.auth import authenticate_user
+from chainlit.utils import mount_chainlit
+
+app = FastAPI()
+
+cookie_scheme = APIKeyCookie(name="access_token")
 
 
-@cl.on_message
-async def on_message(message: cl.Message):
-    get = cl.user_session.get("user")
-    print(get)
-    # Get all the messages in the conversation in the OpenAI format
-    print(cl.chat_context.to_openai())
-
-    # Send the response
-    response = f"Hello, you just sent: {message.content}!"
-    await cl.Message(response).send()
+@app.get("/app")
+def read_main(token: Annotated[str, Depends(cookie_scheme)]):
+    print(token)
+    return {"message": "Hello World from main app"}
 
 
-# TODO: add scopes and set up to use cookie or use header when accessing an API
-# noinspection PyUnusedLocal
-@cl.oauth_callback
-def oauth_callback(
-  provider_id: str,
-  token: str,
-  raw_user_data: Dict[str, str],
-  default_user: cl.User,
-) -> Optional[cl.User]:
-  return default_user
+@app.get("/gdocs")
+async def list_docs(
+    request: Request,
+    current_user: Annotated[
+        Union[cl.User], Depends(authenticate_user)
+    ],
+):
+    # init_http_context(user=current_user)
+    # await cl.Message(content="Hello World").send()
+    token = current_user.metadata.get('token')
+    creds = Credentials(token=token)
+    try:
+        service = build("drive", "v3", credentials=creds)
 
-
-@cl.set_starters
-async def set_starters():
-    return [
-        cl.Starter(
-            label="Example",
-            message="Find the receipt of my favourite apple pie.",
-            icon="/public/example.png"
+        # Call the Drive v3 API
+        results = (
+            service.files()
+            .list(pageSize=10, fields="nextPageToken,files(id,name)")
+            .execute()
         )
-    ]
+        items = results.get("files", [])
+
+        if not items:
+            print("No files found.")
+            return
+        print("Files:")
+        for item in items:
+            print(f"{item['name']} ({item['id']})")
+    except HttpError as error:
+        # TODO(developer) - Handle errors from drive API.
+        print(f"An error occurred: {error}")
+    return HTMLResponse("Hello World")
 
 
-@cl.on_chat_start
-async def start():
-    settings = cl.ChatSettings(
-        [
-            Select(
-                id="Model",
-                label="OpenAI - Model",
-                values=["gpt-3.5-turbo", "gpt-3.5-turbo-16k", "gpt-4", "gpt-4-32k"],
-                initial_index=1,
-            ),
-            Tags(id="StopSequence", label="OpenAI - StopSequence", initial=["uno", "dos"], values=["uno", "dos", "tres"]),
-            Switch(id="Streaming7", label="OpenAI - Stream Tokens", initial=True),
-            Slider(
-                id="Temperature",
-                label="OpenAI - Temperature",
-                initial=0,
-                min=0,
-                max=2,
-                step=0.1,
-            ),
-            Slider(
-                id="SAI_Steps",
-                label="Stability AI - Steps",
-                initial=30,
-                min=10,
-                max=150,
-                step=1,
-                description="Amount of inference steps performed on image generation.",
-            ),
-            Slider(
-                id="SAI_Cfg_Scale",
-                label="Stability AI - Cfg_Scale",
-                initial=7,
-                min=1,
-                max=35,
-                step=0.1,
-                description="Influences how strongly your generation is guided to match your prompt.",
-            ),
-            Slider(
-                id="SAI_Width",
-                label="Stability AI - Image Width",
-                initial=512,
-                min=256,
-                max=2048,
-                step=64,
-                tooltip="Measured in pixels",
-            ),
-            Slider(
-                id="SAI_Height",
-                label="Stability AI - Image Height",
-                initial=512,
-                min=256,
-                max=2048,
-                step=64,
-                tooltip="Measured in pixels",
-            ),
-        ]
-    )
-    await settings.send()
+@app.post("/gdocs/{doc_id}")
+async def upload_doc(
+    doc_id: str,
+    request: Request,
+    current_user: Annotated[
+        Union[cl.User], Depends(authenticate_user)
+    ],
+):
+    token = current_user.metadata.get('token')
+    creds = Credentials(token=token)
+    try:
+        service = build("docs", "v1", credentials=creds)
+        document = service.documents().get(documentId=doc_id).execute()
+        content = document.get('body').get('content')
+        full_text = extract_text(content)
+        print(full_text)
+    except HttpError as err:
+        print(err)
+    return "ok"
+
+
+def extract_text(elements):
+    text = ''
+    for element in elements:
+        if 'paragraph' in element:
+            for run in element['paragraph']['elements']:
+                if 'textRun' in run:
+                    text += run['textRun']['content']
+        elif 'table' in element:
+            for row in element['table']['tableRows']:
+                for cell in row['tableCells']:
+                    text += extract_text(cell['content'])
+        elif 'tableOfContents' in element:
+            text += extract_text(element['tableOfContents']['content'])
+    return text
+
+
+mount_chainlit(app=app, target="chat.py", path="/chat")
